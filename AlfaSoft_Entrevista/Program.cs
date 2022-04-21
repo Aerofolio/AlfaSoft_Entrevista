80a9f48487cd9ae84c41d939e04f3c2c1eadff10
Aerofolio <paulo_bodaneze@hotmail.com>
2022-04-21 11:50:24 -0300

    Added GetFilePath Method

--- AlfaSoft_Entrevista/Program.cs
+++ AlfaSoft_Entrevista/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace AlfaSoft_Entrevista
+﻿using System;
+
+namespace AlfaSoft_Entrevista
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            GetFilePath();
+        }
+
+        protected static string GetFilePath()
+        {
+            Console.WriteLine("Insert the full path to a file containing users:");
+            string filePath = Console.ReadLine();
+
+            return filePath;
         }
     }
 }
