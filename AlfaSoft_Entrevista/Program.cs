8af31fb7c8462e4146618f6c432ac8c7464eded8
Aerofolio <paulo_bodaneze@hotmail.com>
2022-04-21 11:53:53 -0300

    Added GetUsers() Method and base file for testing.

--- AlfaSoft_Entrevista/Program.cs
+++ AlfaSoft_Entrevista/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace AlfaSoft_Entrevista
 {
@@ -6,7 +8,8 @@ namespace AlfaSoft_Entrevista
     {
         static void Main(string[] args)
         {
-            GetFilePath();
+            List<string> users = GetFileAndUsers();
+            Console.ReadLine();
         }
 
         protected static string GetFilePath()
@@ -15,6 +18,35 @@ namespace AlfaSoft_Entrevista
             string filePath = Console.ReadLine();
 
             return filePath;
+        }
+
+        protected static List<string> GetUsers(string filePath)
+        {
+            List<string> users = new List<string>();
+            try
+            {
+                using (var sr = new StreamReader(filePath))
+                {
+                    var fileData = sr.ReadToEnd();
+                    fileData = fileData.Replace("\r\n", "\n");
+                    var fileLines = fileData.Split('\n');
+                    foreach (var line in fileLines)
+                    {
+                        users.Add(line);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(ex.Message);
+            }
+            return users;
+        }
+        protected static List<string> GetFileAndUsers()
+        {
+            string filePath = GetFilePath();
+            return GetUsers(filePath);
         }
     }
 }
